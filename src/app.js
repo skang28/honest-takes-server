9760b7a68baa5fb264cbe063b5a1ed80720ce729
Steven Kang <skang28@gmail.com>
2020-01-20 11:37:59 -0500

    enabling CORS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const {CLIENT_ORIGIN} = require('/config')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
 const postsRouter = require('./posts/posts-router')
@@ -16,7 +17,9 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
 
 app.use('/api/posts', postsRouter)
 app.use('/api/comments', commentsRouter)
